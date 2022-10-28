3168250f10de010f94738de2e65728fdb4d668e6
Jorge Arcia <jorluar372@gmail.com>
2022-10-28 20:38:18 -0300

    updates BO patient/appointments

--- src/app/components/backoffices/patient/patient-appointments/patient-appointments.component.ts
+++ src/app/components/backoffices/patient/patient-appointments/patient-appointments.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PatientTenantsAppointmentsService } from 'src/app/services/patient/patient-tenants-appointments.service';
+import { OperatorTenantsService } from '../../../../services/operator/operator-tenants.service';
+
 
 @Component({
   selector: 'app-patient-appointments',
@@ -10,18 +12,37 @@ import { PatientTenantsAppointmentsService } from 'src/app/services/patient/pati
 export class PatientAppointmentsComponent implements OnInit {
 
   appointments: any[] = [];
+  tenants: any[] = [];
+  selectedTenant: any;
+  selectedEspecialidad: any;
 
   constructor(
     private PatientTenantsAppointmentsService:PatientTenantsAppointmentsService,
+    private operatorTenantsService : OperatorTenantsService,
   ) { }
 
   ngOnInit(): void {
     this.readAppointments();
+    this.readTenants();
   }
 
   private async readAppointments() {
     const {appointments}:any = await this.PatientTenantsAppointmentsService.readAppointments();
     this.appointments = appointments;
   }
+
+  private async readTenants() {
+    const {tenants}:any = await this.operatorTenantsService.readTenants();
+    this.tenants = tenants;
+  }
+
+  onSelectTenant(obj: any): void{
+    this.selectedTenant = obj.detail;
+  }
+
+  onSelectEspecialidad(obj: any): void{
+    this.selectedEspecialidad = obj.detail;
+  }
+
 
 }
--- src/app/components/backoffices/patient/patient-dashboard/patient-dashboard.component.ts
+++ src/app/components/backoffices/patient/patient-dashboard/patient-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PatientTenantsAppointmentsService } from 'src/app/services/patient/patient-tenants-appointments.service';
+
 @Component({
   selector: 'app-patient-dashboard',
   templateUrl: './patient-dashboard.component.html',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientDashboardComponent implements OnInit {
 
-  constructor() { }
+  appointments: any[] = [];
+
+  constructor(
+    private PatientTenantsAppointmentsService: PatientTenantsAppointmentsService,
+  ) { }
 
   ngOnInit(): void {
+    this.readAppointments();
+  }
+
+  private async readAppointments() {
+    const {appointments}:any = await this.PatientTenantsAppointmentsService.readAppointments();
+    this.appointments = appointments;
   }
 
 }
